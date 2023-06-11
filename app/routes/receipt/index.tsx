febdb58f60b2c217ae7772ad8b101b5875eda307
Ardi Nursyamsu <ardinursyamsu@hotmail.com>
2023-06-11 18:40:08 +0700

    Fix Create and Display Receipt page

--- app/routes/receipt/index.tsx
+++ app/routes/receipt/index.tsx
@@ -1,16 +1,52 @@
 import { json } from "@remix-run/node";
-import { TRX_SOURCE_RECEIPT } from "assets/helper/constants";
+import { Link, useLoaderData } from "@remix-run/react";
+import { ACT_CASH, TRX_SOURCE_RECEIPT } from "assets/helper/constants";
+import {
+  displayCapitalFirst,
+  displayDate,
+  formatter,
+} from "assets/helper/helper";
 import Body from "assets/layouts/body";
 import ReceiptNavbar from "assets/layouts/customnavbar/receipt-navbar";
-import { getAllTransactionBySource } from "~/models/transaction.server";
+import {
+  getAllTransactionBySource,
+  getTransactionsByOrderIdAndTransactionSource,
+} from "~/models/transaction.server";
 
 export const loader = async () => {
   const data = await getAllTransactionBySource(TRX_SOURCE_RECEIPT);
-  
-  return json(data)
-}
+
+  var receiptTrxData = [];
+
+  for (const trx of data) {
+    const receiptPerOrderID =
+      await getTransactionsByOrderIdAndTransactionSource(
+        TRX_SOURCE_RECEIPT,
+        trx.orderId
+      );
+
+    const trxTime = trx.trxTime;
+    const refId = trx.sourceTrx.toString() + "-" + trx.orderId.toString();
+    var remark = "";
+    const user = trx.userId;
+    var totalAmount = 0;
+    for (const receiptCtrl of receiptPerOrderID) {
+      if (receiptCtrl.accountId == ACT_CASH) {
+        totalAmount += Number(receiptCtrl.amount);
+      } else {
+        remark += receiptCtrl.accountId + ", ";
+      }
+    }
+
+    receiptTrxData.push({ trxTime, refId, remark, user, totalAmount });
+  }
+
+  return json({ receiptTrxData });
+};
 
 export default function ReceiptIndex() {
+  const { receiptTrxData } = useLoaderData<typeof loader>();
+
   return (
     <Body>
       <ReceiptNavbar />
@@ -28,11 +64,8 @@ export default function ReceiptIndex() {
                 <th className="text-center col-2" scope="col">
                   Ref ID
                 </th>
-                <th className="text-center col-2" scope="col">
-                  Account
-                </th>
-                <th className="text-center col-2" scope="col">
-                  Sub-Account
+                <th className="text-center col-4" scope="col">
+                  Remark
                 </th>
                 <th className="text-center col-1" scope="col">
                   User
@@ -42,7 +75,32 @@ export default function ReceiptIndex() {
                 </th>
               </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {receiptTrxData.map((receipt, idx) => (
+                <tr key={idx + 1}>
+                  <th className="text-center" scope="row">
+                    {idx + 1}
+                  </th>
+                  <td className="text-center">
+                    {displayDate(receipt.trxTime)}
+                  </td>
+                  <td className="text-center">
+                    <Link to={receipt.refId}>
+                      {receipt.refId.toUpperCase()}
+                    </Link>
+                  </td>
+                  <td className="text-start">
+                    {displayCapitalFirst(receipt.remark).replaceAll("-", " ")}
+                  </td>
+                  <td className="text-end">
+                    {displayCapitalFirst(receipt.user)}
+                  </td>
+                  <td className="text-end">
+                    {formatter.format(receipt.totalAmount)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
